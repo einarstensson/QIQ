d70b56bf4787e7b9b53ba714d7c017bbd5177653
Lamine Ndaw <brahimndaw@gmail.com>
2016-06-15 17:32:12 -0400

    15 Questions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,29 @@
+question1= Question.create(content: "Tell me about yourself?")
 
+question3= Question.create(content:"What are your strengths and weaknesses?")
+
+question5= Question.create(content: "Tell me about the most recent project you worked on. What were your responsibilities?")
+
+question7= Question.create(content:"What is function hoisting in JavaScript?")
+
+question9= Question.create(content:"What is a closure?")
+
+question11= Question.create(content: "Describe a time when you were able to improve upon the design that was originally suggested?")
+
+question13= Question.create(content: "Where would you like to be in your career five years from now?")
+
+question15= Question.create(content: "Can you name two programming paradigms important for JavaScript app developers?")
+
+question17= Question.create(content: "Tell me about the most recent project you worked on. What were your responsibilities?")
+
+question19= Question.create(content: "Do you have any questions for me??")
+
+question21= Question.create(content: "What is the difference between classical inheritance and prototypal inheritance? ")
+
+question23= Question.create(content: "Explain a polymorphic association?")
+
+question25= Question.create(content: "Describe your production deployment process?")
+
+question27= Question.create(content: "How would you declare and use a constructor in Ruby?")
+
+question29= Question.create(content: "Do you have any questions for me?")
