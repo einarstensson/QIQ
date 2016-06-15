7ad3e5e13617d2e8d731c6f3ae280213c5334fb0
einarstensson <einar.stensson@gmail.com>
2016-06-15 17:14:05 -0400

    Added even numbered interview questions for the seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,30 @@
 
+question_two = Question.create(content: "How many ways can you center a div?")
+
+question_four = Question.create(content: "Explain the process of web development to a middle school kid.")
+
+question_six = Question.create(content: "How does the web work?")
+
+question_eight = Question.create(content: "How did you learn programming?")
+
+question_ten = Question.create(content: "How did you learn web development?")
+
+question_twelve = Question.create(content: "So tell me about yourself.")
+
+question_fourteen = Question.create(content: "Why do you want to work with us?")
+
+question_sixteen = Question.create(content: "Tell me about your long-term goals.")
+
+question_eighteen = Question.create(content: "What are your short-term goals.")
+
+question_twenty = Question.create(content: "Please walk me through one of the projects on your resume.")
+
+question_twentytwo = Question.create(content: "Can you tell me about a challenge you've faced at work and how you got through it?")
+
+question_twentyfour = Question.create(content: "Why do you want to be a web developer?")
+
+question_twentysix = Question.create(content: "Why do you want to work for us?")
+
+question_twentyeight = Question.create(content: "Do you have any questions for me?")
+
+question_thirty = Question.create(content: "How do you evaluate success?")
