8272a7211681cc1aa670a7e779f519ea890fc964
einarstensson <einar.stensson@gmail.com>
2016-06-15 18:11:43 -0400

    Resolve duplicate questions and add new ones

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,61 +1,30 @@
-question1= Question.create(content: "Tell me about yourself?")
-
-
-question3= Question.create(content:"What are your strengths and weaknesses?")
-
-question5= Question.create(content: "Tell me about the most recent project you worked on. What were your responsibilities?")
-
-question7= Question.create(content:"What is function hoisting in JavaScript?")
-
-question9= Question.create(content:"What is a closure?")
-
-question11= Question.create(content: "Describe a time when you were able to improve upon the design that was originally suggested?")
-
-question13= Question.create(content: "Where would you like to be in your career five years from now?")
-
-question15= Question.create(content: "Can you name two programming paradigms important for JavaScript app developers?")
-
-question17= Question.create(content: "Tell me about the most recent project you worked on. What were your responsibilities?")
-
-question19= Question.create(content: "Do you have any questions for me??")
-
-question21= Question.create(content: "What is the difference between classical inheritance and prototypal inheritance? ")
-
-question23= Question.create(content: "Explain a polymorphic association?")
-
-question25= Question.create(content: "Describe your production deployment process?")
-
-question27= Question.create(content: "How would you declare and use a constructor in Ruby?")
-
-question29= Question.create(content: "Do you have any questions for me?")
-
+question_one = Question.create(content: "What are you looking for in terms of career development?")
 question_two = Question.create(content: "How many ways can you center a div?")
-
+question_three= Question.create(content:"What are your strengths and weaknesses?")
 question_four = Question.create(content: "Explain the process of web development to a middle school kid.")
-
+question_five = Question.create(content: "What did you like least about your last job?")
 question_six = Question.create(content: "How does the web work?")
-
+question_seven= Question.create(content:"What is function hoisting in JavaScript?")
 question_eight = Question.create(content: "How did you learn programming?")
-
+question_nine= Question.create(content:"What is a closure?")
 question_ten = Question.create(content: "How did you learn web development?")
-
+question_eleven= Question.create(content: "Describe a time when you were able to improve upon the design that was originally suggested?")
 question_twelve = Question.create(content: "So tell me about yourself.")
-
+question_thirteen= Question.create(content: "Where would you like to be in your career five years from now?")
 question_fourteen = Question.create(content: "Why do you want to work with us?")
-
+question_fifteen= Question.create(content: "Can you name two programming paradigms important for JavaScript app developers?")
 question_sixteen = Question.create(content: "Tell me about your long-term goals.")
-
+question_seventeen= Question.create(content: "Tell me about the most recent project you worked on. What were your responsibilities?")
 question_eighteen = Question.create(content: "What are your short-term goals.")
-
+question_nineteen= Question.create(content: "Do you have any questions for me?")
 question_twenty = Question.create(content: "Please walk me through one of the projects on your resume.")
-
+question_twentyone= Question.create(content: "What is the difference between classical inheritance and prototypal inheritance? ")
 question_twentytwo = Question.create(content: "Can you tell me about a challenge you've faced at work and how you got through it?")
-
+question_twentythree= Question.create(content: "Explain a polymorphic association?")
 question_twentyfour = Question.create(content: "Why do you want to be a web developer?")
-
+question_twentyfive= Question.create(content: "Describe your production deployment process?")
 question_twentysix = Question.create(content: "Why do you want to work for us?")
-
+question_twentyseven= Question.create(content: "How would you declare and use a constructor in Ruby?")
 question_twentyeight = Question.create(content: "Do you have any questions for me?")
-
-question_thirty = Question.create(content: "How do you evaluate success?")
-
+question_twentynine = Question.create(content: "How do you evaluate success?")
+question_thirty = Question.create(content: "How would you describe your work style?")
