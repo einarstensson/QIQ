2bd825acfd3522fcf4cad1a072c35701a8fa37e1
einarstensson <einar.stensson@gmail.com>
2016-06-20 11:18:09 -0400

    Major changes

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -3,28 +3,31 @@ module Api
   module V1
     class AnswersController < ApplicationController
 
-      def index
-        render json: Answer.all
-      end
-
       def create
+        debugger
         content = answer_params['content']
         question_id = question_id_params['id']
         question = Question.find_by(id: question_id)
-        question.answers.new(content: content)
-        question.save
+        question.answers.create(content: content)
+
+        render json: question.answers.last
       end
 
-      def call
-        debugger
-      end
-
+      #for the up/downvoting
       def update
         debugger
+        answer = Answer.find(params[:id])
+        answer.update(vote_count: params[:data][:attributes]['vote-count'])
+        render json: answer
+      end
+
+      def show
+        render json: Answer.find(params[:id])
       end
 
       private
 
+      #update the params
       def answer_params
         params.require(:data).require(:attributes).permit(:content, :vote_count, :vote_score)
       end
--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -2,23 +2,14 @@ module Api
   module V1
     class QuestionsController < ApplicationController
 
-      def index
-        #append length attribute
-        render json: Question.includes(:answers).all, include: ['answers']
-      end
-
       def show
         question = Question.find_by(id: params[:id])
         render json: question, include: ['answers']
       end
 
-      def update
-        debugger
-      end
-
       def getRandomQuestion
-        question = Question.all.sample
-        render json: question, include: ['answers']
+        id = rand(0..Question.all.size) + 1
+        render json: {id: id}
       end
 
     end
